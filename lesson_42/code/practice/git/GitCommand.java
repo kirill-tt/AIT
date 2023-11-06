11a6c0a19c972143a3dfe8c2048829171c32e197
Kirill Bott <kes99@mail.ru>
2023-11-06 11:34:11 +0100

    upg

--- lesson_42/code/practice/git/GitCommand.java
+++ lesson_42/code/practice/git/GitCommand.java
@@ -1,10 +1,17 @@
 package practice.git;
 
 public class GitCommand {
-    //git add .
-   // git commit -m 'update'
-    //git push -u origin main
+    //git add .// добавить
+   // git commit -m 'update'// сохранить
+    //git push -u origin main// выгрузить
     // git add .
     // git commit -m 'update'
     // git push -u origin main
+    // git branch // просмотр веток
+    // git branch новая_ветка // создать новую ветку
+    // git checkout название_ветки // переключиться на другую ветку
+// git checkout -// переключение между ветками
+// git checkout -b new_branch
+    // git checkout -b new_branch main
+
 }
