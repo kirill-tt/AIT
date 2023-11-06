1282171f5ebbda7d0f4617829bc3977cfa023279
Kirill Bott <kes99@mail.ru>
2023-11-06 10:42:29 +0100

    update

--- lesson_42/code/practice/git/GitCommand.java
+++ lesson_42/code/practice/git/GitCommand.java
@@ -4,4 +4,7 @@ public class GitCommand {
     //git add .
    // git commit -m 'update'
     //git push -u origin main
+    // git add .
+    // git commit -m 'update'
+    // git push -u origin main
 }
